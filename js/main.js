6fcc1d1980471586fb90a29a07712bfe984266c2
Andrey-Demkovich <a.demkovich@tut.by>
2019-06-14 17:13:01 +0300

    Исправил ошибки eslint по кавычкам двойным

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-"use strict";
+'use strict';
 
 var LIKES_MIN = 15;
 var LIKES_MAX = 200;
 
 var URLS = [];
 for (var i = 1; i <= 25; i++) {
-  URLS.push("photos/" + i + ".jpg");
+  URLS.push('photos/' + i + '.jpg');
 }
 
 var COMMENTS = [
-  "Всё отлично!",
-  "В целом всё неплохо. Но не всё.",
-  "Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.",
-  "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
-  "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
-  "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
 var COMMENTATORS = [
-  { name: "Андрей", avatar: "img/avatar-1.jpg" },
-  { name: "Геннадий", avatar: "img/avatar-2.jpg" },
-  { name: "Игорь", avatar: "img/avatar-3.jpg" },
-  { name: "Виктор", avatar: "img/avatar-4.jpg" },
-  { name: "Екатерина", avatar: "img/avatar-5.jpg" },
-  { name: "Елена", avatar: "img/avatar-6.jpg" }
+  {name: 'Андрей', avatar: 'img/avatar-1.jpg'},
+  {name: 'Геннадий', avatar: 'img/avatar-2.jpg'},
+  {name: 'Игорь', avatar: 'img/avatar-3.jpg'},
+  {name: 'Виктор', avatar: 'img/avatar-4.jpg'},
+  {name: 'Екатерина', avatar: 'img/avatar-5.jpg'},
+  {name: 'Елена', avatar: 'img/avatar-6.jpg'}
 ];
 
 var picterTemplate = document
-  .querySelector("#picture")
-  .content.querySelector(".picture");
+  .querySelector('#picture')
+  .content.querySelector('.picture');
 
-var picturesContainerElement = document.querySelector(".pictures");
+var picturesContainerElement = document.querySelector('.pictures');
 
-var generateRandomInteger = function(min, max) {
+var generateRandomInteger = function (min, max) {
   return min + Math.floor((max + 1 - min) * Math.random());
 };
 
-var generatePosts = function(urls, comments, comentators) {
+var generatePosts = function (urls, comments, comentators) {
   var posts = [];
   var urlsClone = urls.slice();
   for (var i = 0; i < urls.length; i++) {
@@ -53,8 +53,8 @@ var generatePosts = function(urls, comments, comentators) {
     var commentsClone = comments.slice();
     for (var j = 0; j < generateRandomInteger(2, 5); j++) {
       var commentator = commentorsClone.splice(
-        generateRandomInteger(0, commentorsClone.length - 1),
-        1
+          generateRandomInteger(0, commentorsClone.length - 1),
+          1
       );
 
       posts[i].comments[j] = {};
@@ -69,18 +69,18 @@ var generatePosts = function(urls, comments, comentators) {
   return posts;
 };
 
-var createPicterPost = function(post) {
+var createPicterPost = function (post) {
   var picterElement = picterTemplate.cloneNode(true);
 
-  picterElement.querySelector(".picture__img").src = post.url;
-  picterElement.querySelector(".picture__likes").textContent = post.likes;
-  picterElement.querySelector(".picture__comments").textContent =
+  picterElement.querySelector('.picture__img').src = post.url;
+  picterElement.querySelector('.picture__likes').textContent = post.likes;
+  picterElement.querySelector('.picture__comments').textContent =
     post.comments.length;
 
   return picterElement;
 };
 
-var insertFragment = function(posts) {
+var insertFragment = function (posts) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < posts.length; i++) {
     fragment.appendChild(createPicterPost(posts[i]));
