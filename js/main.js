f4513289ed98f3f43f5a0206a3037cb77547b6ab
Andrey-Demkovich <a.demkovich@tut.by>
2019-06-22 21:42:16 +0300

    Создал модуль effects-change.js

--- js/main.js
+++ js/main.js
@@ -220,41 +220,45 @@
 })();
 
 // 2.2. Наложение эффекта на изображение:
+
+// Файл-модуль effects-change.js
 //  2.2.1. Смена эффекта
-var changeEffectsPreview = function (evt) {
-  window.imageUploadPreviewElement.className = '';
-  if (evt.target.value !== 'none') {
-    window.effectLevel.classList.remove('hidden');
-    window.imageUploadPreviewElement.classList.add(
-        'effects__preview--' + evt.target.value
-    );
-  }
+(function () {
+  var changeEffectsPreview = function (evt) {
+    window.imageUploadPreviewElement.className = '';
+    if (evt.target.value !== 'none') {
+      window.effectLevel.classList.remove('hidden');
+      window.imageUploadPreviewElement.classList.add(
+          'effects__preview--' + evt.target.value
+      );
+    }
 
-  if (evt.target.value === 'none') {
-    window.imageUploadPreviewElement.style.filter = '';
-    window.effectLevel.classList.add('hidden');
-  } else if (evt.target.value === 'chrome') {
-    window.imageUploadPreviewElement.style.filter = 'grayscale(1)';
-  } else if (evt.target.value === 'sepia') {
-    window.imageUploadPreviewElement.style.filter = 'sepia(1)';
-  } else if (evt.target.value === 'marvin') {
-    window.imageUploadPreviewElement.style.filter = 'invert(100%)';
-  } else if (evt.target.value === 'phobos') {
-    window.imageUploadPreviewElement.style.filter = 'blur(3px)';
-  } else if (evt.target.value === 'heat') {
-    window.imageUploadPreviewElement.style.filter = 'brightness(3)';
-  }
+    if (evt.target.value === 'none') {
+      window.imageUploadPreviewElement.style.filter = '';
+      window.effectLevel.classList.add('hidden');
+    } else if (evt.target.value === 'chrome') {
+      window.imageUploadPreviewElement.style.filter = 'grayscale(1)';
+    } else if (evt.target.value === 'sepia') {
+      window.imageUploadPreviewElement.style.filter = 'sepia(1)';
+    } else if (evt.target.value === 'marvin') {
+      window.imageUploadPreviewElement.style.filter = 'invert(100%)';
+    } else if (evt.target.value === 'phobos') {
+      window.imageUploadPreviewElement.style.filter = 'blur(3px)';
+    } else if (evt.target.value === 'heat') {
+      window.imageUploadPreviewElement.style.filter = 'brightness(3)';
+    }
 
-  // При смене эффекта устаналиваем ползунок слайдера на 100%
-  window.effectLevelPinElement.style.left = '100%';
-  window.effectLevelDepthElement.style.width = '100%';
-};
+    // При смене эффекта устаналиваем ползунок слайдера на 100%
+    window.effectLevelPinElement.style.left = '100%';
+    window.effectLevelDepthElement.style.width = '100%';
+  };
 
-window.effectsRadioElements.forEach(function (item) {
-  item.addEventListener('change', function (evt) {
-    changeEffectsPreview(evt);
+  window.effectsRadioElements.forEach(function (item) {
+    item.addEventListener('change', function (evt) {
+      changeEffectsPreview(evt);
+    });
   });
-});
+})();
 
 //  2.2.2. Изменение интенсивности эффекта
 var changeIntensityEffect = function () {
