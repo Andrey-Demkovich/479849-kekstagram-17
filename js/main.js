a99cac39475f837362bae40c5fbe24281901562f
Andrey-Demkovich <a.demkovich@tut.by>
2019-06-22 22:30:09 +0300

    Создал модуль zoom.js

--- js/main.js
+++ js/main.js
@@ -369,45 +369,48 @@
   window.effectLevel.addEventListener('click', window.changeIntensityEffect);
 })();
 
+// Создал файл-модуль zoom.js
 // 2.1. Масштаб:
-var MIN_SCALE = 0;
-var MAX_SCALE = 100;
-var STEP = 25;
-var scaleControlSmaller = window.imgUploadOverlayElement.querySelector(
-    '.scale__control--smaller'
-);
-var scaleControlBigger = window.imgUploadOverlayElement.querySelector(
-    '.scale__control--bigger'
-);
+(function () {
+  var MIN_SCALE = 0;
+  var MAX_SCALE = 100;
+  var STEP = 25;
+  var scaleControlSmaller = window.imgUploadOverlayElement.querySelector(
+      '.scale__control--smaller'
+  );
+  var scaleControlBigger = window.imgUploadOverlayElement.querySelector(
+      '.scale__control--bigger'
+  );
 
-var outZoom = function () {
-  var scaleSmaller = parseInt(window.scaleCntrolValue.value, 10) - STEP;
+  var outZoom = function () {
+    var scaleSmaller = parseInt(window.scaleCntrolValue.value, 10) - STEP;
 
-  if (scaleSmaller <= MIN_SCALE) {
-    scaleSmaller = MIN_SCALE;
-  }
-  window.scaleCntrolValue.value = scaleSmaller + '%';
+    if (scaleSmaller <= MIN_SCALE) {
+      scaleSmaller = MIN_SCALE;
+    }
+    window.scaleCntrolValue.value = scaleSmaller + '%';
 
-  window.imageUploadPreviewElement.style.transform =
-    'scale(' + scaleSmaller / 100 + ')';
-};
+    window.imageUploadPreviewElement.style.transform =
+      'scale(' + scaleSmaller / 100 + ')';
+  };
 
-var inZoom = function () {
-  var scaleBigger = parseInt(window.scaleCntrolValue.value, 10) + STEP;
+  var inZoom = function () {
+    var scaleBigger = parseInt(window.scaleCntrolValue.value, 10) + STEP;
 
-  if (scaleBigger >= MAX_SCALE) {
-    scaleBigger = MAX_SCALE;
-  }
-  window.scaleCntrolValue.value = scaleBigger + '%';
+    if (scaleBigger >= MAX_SCALE) {
+      scaleBigger = MAX_SCALE;
+    }
+    window.scaleCntrolValue.value = scaleBigger + '%';
 
-  window.imageUploadPreviewElement.style.transform =
-    'scale(' + scaleBigger / 100 + ')';
-};
+    window.imageUploadPreviewElement.style.transform =
+      'scale(' + scaleBigger / 100 + ')';
+  };
 
-scaleControlSmaller.addEventListener('click', function () {
-  outZoom();
-});
+  scaleControlSmaller.addEventListener('click', function () {
+    outZoom();
+  });
 
-scaleControlBigger.addEventListener('click', function () {
-  inZoom();
-});
+  scaleControlBigger.addEventListener('click', function () {
+    inZoom();
+  });
+})();
