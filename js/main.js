a442f8478d4250a283a74ea7fd6cedc41fce818d
Andrey-Demkovich <a.demkovich@tut.by>
2019-06-22 21:23:59 +0300

    Создал модуль dom-element-query.js

--- js/main.js
+++ js/main.js
@@ -121,153 +121,179 @@
   insertFragment(window.pictersPosts);
 })();
 
-// Загрузка изображения и показ формы редактирования
-var ESK_KEYCODE = 27;
+// Файл-модуль dom-element-query.js
+// Задает глобально частоиспользуемые DOM-элементы
+(function () {
+  window.imgUploadOverlayElement = document.querySelector(
+      '.img-upload__overlay'
+  );
 
-var uploadFileElement = document.querySelector('#upload-file');
-var imgUploadOverlayElement = document.querySelector('.img-upload__overlay');
-var uploadCancelElement = imgUploadOverlayElement.querySelector(
-    '#upload-cancel'
-);
-var effectLevel = imgUploadOverlayElement.querySelector('.effect-level');
-var effectsRadioElements = imgUploadOverlayElement.querySelectorAll(
-    '.effects__radio'
-);
-var imgUploadPreviewElement = imgUploadOverlayElement.querySelector(
-    '.img-upload__preview'
-);
-var imageUploadPreviewElement = imgUploadPreviewElement.firstElementChild;
-var scaleCntrolValue = imgUploadOverlayElement.querySelector(
-    '.scale__control--value'
-);
-var textDescriptionElement = imgUploadOverlayElement.querySelector(
-    '.text__description'
-);
+  window.effectLevel = window.imgUploadOverlayElement.querySelector(
+      '.effect-level'
+  );
+  window.effectLevelValueElement = window.effectLevel.querySelector(
+      '.effect-level__value'
+  );
+  window.effectLevelLineElement = window.effectLevel.querySelector(
+      '.effect-level__line'
+  );
+  window.effectLevelPinElement = window.effectLevel.querySelector(
+      '.effect-level__pin'
+  );
+  window.effectLevelDepthElement = window.effectLevel.querySelector(
+      '.effect-level__depth'
+  );
 
-var onImgUploadEscPress = function (evt) {
-  if (evt.keyCode === ESK_KEYCODE) {
-    closeImgUpload();
-  }
-};
+  window.effectsRadioElements = window.imgUploadOverlayElement.querySelectorAll(
+      '.effects__radio'
+  );
+  window.imgUploadPreviewElement = window.imgUploadOverlayElement.querySelector(
+      '.img-upload__preview'
+  );
+  window.imageUploadPreviewElement =
+    window.imgUploadPreviewElement.firstElementChild;
+  window.scaleCntrolValue = window.imgUploadOverlayElement.querySelector(
+      '.scale__control--value'
+  );
+})();
 
-var closeImgUpload = function () {
-  imgUploadOverlayElement.classList.add('hidden');
+// Файл-модуль form-open-close.js
+// Загрузка изображения и показ(скрытие) формы редактирования
+(function () {
+  var ESK_KEYCODE = 27;
 
-  document.removeEventListener('keydown', onImgUploadEscPress);
+  var uploadFileElement = document.querySelector('#upload-file');
 
-  imageUploadPreviewElement.className = '';
-  imageUploadPreviewElement.style.filter = '';
-  imageUploadPreviewElement.style.transform = 'scale(1)';
-};
+  var uploadCancelElement = window.imgUploadOverlayElement.querySelector(
+      '#upload-cancel'
+  );
+  var textDescriptionElement = window.imgUploadOverlayElement.querySelector(
+      '.text__description'
+  );
 
-var openImgUpload = function () {
-  imgUploadOverlayElement.classList.remove('hidden');
+  var onImgUploadEscPress = function (evt) {
+    if (evt.keyCode === ESK_KEYCODE) {
+      closeImgUpload();
+    }
+  };
 
-  effectsRadioElements[0].checked = true;
-  scaleCntrolValue.value = '100%';
+  var closeImgUpload = function () {
+    window.imgUploadOverlayElement.classList.add('hidden');
 
-  // Скрываем слайдер эффектов и устанавливаем на 100%
-  effectLevel.classList.add('hidden');
-  effectLevelValueElement.value = 100;
-  effectLevelPinElement.style.left = '100%';
-  effectLevelDepthElement.style.width = '100%';
-
-  document.addEventListener('keydown', onImgUploadEscPress);
-  //   2.4. Комментарий:
-  //   - если фокус находится в поле ввода комментария, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
-  textDescriptionElement.addEventListener('focus', function () {
     document.removeEventListener('keydown', onImgUploadEscPress);
-  });
-  textDescriptionElement.addEventListener('blur', function () {
+
+    window.imageUploadPreviewElement.className = '';
+    window.imageUploadPreviewElement.style.filter = '';
+    window.imageUploadPreviewElement.style.transform = 'scale(1)';
+  };
+
+  var openImgUpload = function () {
+    window.imgUploadOverlayElement.classList.remove('hidden');
+
+    window.effectsRadioElements[0].checked = true;
+    window.scaleCntrolValue.value = '100%';
+
+    // Скрываем слайдер эффектов и устанавливаем на 100%
+    window.effectLevel.classList.add('hidden');
+    window.effectLevelValueElement.value = 100;
+    window.effectLevelPinElement.style.left = '100%';
+    window.effectLevelDepthElement.style.width = '100%';
+
     document.addEventListener('keydown', onImgUploadEscPress);
-  });
+    //   2.4. Комментарий:
+    //   - если фокус находится в поле ввода комментария, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
+    textDescriptionElement.addEventListener('focus', function () {
+      document.removeEventListener('keydown', onImgUploadEscPress);
+    });
+    textDescriptionElement.addEventListener('blur', function () {
+      document.addEventListener('keydown', onImgUploadEscPress);
+    });
 
-  uploadCancelElement.addEventListener('click', function () {
-    closeImgUpload();
-  });
-};
+    uploadCancelElement.addEventListener('click', function () {
+      closeImgUpload();
+    });
+  };
 
-uploadFileElement.addEventListener('change', function () {
-  openImgUpload();
-});
+  uploadFileElement.addEventListener('change', function () {
+    openImgUpload();
+  });
+})();
 
 // 2.2. Наложение эффекта на изображение:
 //  2.2.1. Смена эффекта
 var changeEffectsPreview = function (evt) {
-  imageUploadPreviewElement.className = '';
+  window.imageUploadPreviewElement.className = '';
   if (evt.target.value !== 'none') {
-    effectLevel.classList.remove('hidden');
-    imageUploadPreviewElement.classList.add(
+    window.effectLevel.classList.remove('hidden');
+    window.imageUploadPreviewElement.classList.add(
         'effects__preview--' + evt.target.value
     );
   }
 
   if (evt.target.value === 'none') {
-    imageUploadPreviewElement.style.filter = '';
-    effectLevel.classList.add('hidden');
+    window.imageUploadPreviewElement.style.filter = '';
+    window.effectLevel.classList.add('hidden');
   } else if (evt.target.value === 'chrome') {
-    imageUploadPreviewElement.style.filter = 'grayscale(1)';
+    window.imageUploadPreviewElement.style.filter = 'grayscale(1)';
   } else if (evt.target.value === 'sepia') {
-    imageUploadPreviewElement.style.filter = 'sepia(1)';
+    window.imageUploadPreviewElement.style.filter = 'sepia(1)';
   } else if (evt.target.value === 'marvin') {
-    imageUploadPreviewElement.style.filter = 'invert(100%)';
+    window.imageUploadPreviewElement.style.filter = 'invert(100%)';
   } else if (evt.target.value === 'phobos') {
-    imageUploadPreviewElement.style.filter = 'blur(3px)';
+    window.imageUploadPreviewElement.style.filter = 'blur(3px)';
   } else if (evt.target.value === 'heat') {
-    imageUploadPreviewElement.style.filter = 'brightness(3)';
+    window.imageUploadPreviewElement.style.filter = 'brightness(3)';
   }
 
   // При смене эффекта устаналиваем ползунок слайдера на 100%
-  effectLevelPinElement.style.left = '100%';
-  effectLevelDepthElement.style.width = '100%';
+  window.effectLevelPinElement.style.left = '100%';
+  window.effectLevelDepthElement.style.width = '100%';
 };
 
-effectsRadioElements.forEach(function (item) {
+window.effectsRadioElements.forEach(function (item) {
   item.addEventListener('change', function (evt) {
     changeEffectsPreview(evt);
   });
 });
 
 //  2.2.2. Изменение интенсивности эффекта
-var effectLevelValueElement = effectLevel.querySelector('.effect-level__value');
-var effectLevelLineElement = effectLevel.querySelector('.effect-level__line');
-var effectLevelPinElement = effectLevel.querySelector('.effect-level__pin');
-var effectLevelDepthElement = effectLevel.querySelector('.effect-level__depth');
-
 var changeIntensityEffect = function () {
-  effectLevelValueElement.value =
+  window.effectLevelValueElement.value =
     (
-      effectLevelPinElement.offsetLeft / effectLevelLineElement.clientWidth
+      window.effectLevelPinElement.offsetLeft /
+      window.effectLevelLineElement.clientWidth
     ).toFixed(2) * 100;
 
-  if (imageUploadPreviewElement.classList[0] === 'effects__preview--chrome') {
-    imageUploadPreviewElement.style.filter =
-      'grayscale(' + effectLevelValueElement.value / 100 + ')';
-  } else if (
-    imageUploadPreviewElement.classList[0] === 'effects__preview--sepia'
+  if (
+    window.imageUploadPreviewElement.classList[0] === 'effects__preview--chrome'
   ) {
-    imageUploadPreviewElement.style.filter =
-      'sepia(' + effectLevelValueElement.value / 100 + ')';
+    window.imageUploadPreviewElement.style.filter =
+      'grayscale(' + window.effectLevelValueElement.value / 100 + ')';
   } else if (
-    imageUploadPreviewElement.classList[0] === 'effects__preview--marvin'
+    window.imageUploadPreviewElement.classList[0] === 'effects__preview--sepia'
   ) {
-    imageUploadPreviewElement.style.filter =
-      'invert(' + effectLevelValueElement.value + '%)';
+    window.imageUploadPreviewElement.style.filter =
+      'sepia(' + window.effectLevelValueElement.value / 100 + ')';
   } else if (
-    imageUploadPreviewElement.classList[0] === 'effects__preview--phobos'
+    window.imageUploadPreviewElement.classList[0] === 'effects__preview--marvin'
   ) {
-    imageUploadPreviewElement.style.filter =
-      'blur(' + (effectLevelValueElement.value / 100) * 3 + 'px)';
+    window.imageUploadPreviewElement.style.filter =
+      'invert(' + window.effectLevelValueElement.value + '%)';
   } else if (
-    imageUploadPreviewElement.classList[0] === 'effects__preview--heat'
+    window.imageUploadPreviewElement.classList[0] === 'effects__preview--phobos'
   ) {
-    imageUploadPreviewElement.style.filter =
-      'brightness(' + (effectLevelValueElement.value / 100) * 3 + ')';
+    window.imageUploadPreviewElement.style.filter =
+      'blur(' + (window.effectLevelValueElement.value / 100) * 3 + 'px)';
+  } else if (
+    window.imageUploadPreviewElement.classList[0] === 'effects__preview--heat'
+  ) {
+    window.imageUploadPreviewElement.style.filter =
+      'brightness(' + (window.effectLevelValueElement.value / 100) * 3 + ')';
   }
 };
 
 // Перетаскивание слайдера эффектов
-effectLevelPinElement.addEventListener('mousedown', function (evt) {
+window.effectLevelPinElement.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
 
   var startCoordsX = evt.clientX;
@@ -278,18 +304,20 @@ effectLevelPinElement.addEventListener('mousedown', function (evt) {
     var shiftX = startCoordsX - moveEvt.clientX;
     startCoordsX = moveEvt.clientX;
 
-    var pinElementLeft = effectLevelPinElement.offsetLeft - shiftX;
+    var pinElementLeft = window.effectLevelPinElement.offsetLeft - shiftX;
 
-    var lineElementLeft = effectLevelLineElement.getBoundingClientRect().left;
-    var lineElementRight = effectLevelLineElement.getBoundingClientRect().right;
+    var lineElementLeft = window.effectLevelLineElement.getBoundingClientRect()
+      .left;
+    var lineElementRight = window.effectLevelLineElement.getBoundingClientRect()
+      .right;
     if (startCoordsX <= lineElementLeft) {
       pinElementLeft = 0;
     } else if (startCoordsX >= lineElementRight) {
-      pinElementLeft = effectLevelLineElement.clientWidth;
+      pinElementLeft = window.effectLevelLineElement.clientWidth;
     }
 
-    effectLevelPinElement.style.left = pinElementLeft + 'px';
-    effectLevelDepthElement.style.width = pinElementLeft + 'px';
+    window.effectLevelPinElement.style.left = pinElementLeft + 'px';
+    window.effectLevelDepthElement.style.width = pinElementLeft + 'px';
   };
 
   var onMouseUp = function (upEvt) {
@@ -306,57 +334,59 @@ effectLevelPinElement.addEventListener('mousedown', function (evt) {
 });
 
 // Изменение положения и интенсивности эффекта по клику на слайдер
-effectLevel.addEventListener('click', function (clickEvt) {
+window.effectLevel.addEventListener('click', function (clickEvt) {
   var coordsX = clickEvt.clientX;
-  var lineElementLeft = effectLevelLineElement.getBoundingClientRect().left;
-  var lineElementRight = effectLevelLineElement.getBoundingClientRect().right;
+  var lineElementLeft = window.effectLevelLineElement.getBoundingClientRect()
+    .left;
+  var lineElementRight = window.effectLevelLineElement.getBoundingClientRect()
+    .right;
 
   var elementLeft = coordsX - lineElementLeft;
 
   if (coordsX <= lineElementLeft) {
     elementLeft = 0;
   } else if (coordsX >= lineElementRight) {
-    elementLeft = effectLevelLineElement.clientWidth;
+    elementLeft = window.effectLevelLineElement.clientWidth;
   }
 
-  effectLevelPinElement.style.left = elementLeft + 'px';
-  effectLevelDepthElement.style.width = elementLeft + 'px';
+  window.effectLevelPinElement.style.left = elementLeft + 'px';
+  window.effectLevelDepthElement.style.width = elementLeft + 'px';
 });
 
-effectLevel.addEventListener('click', changeIntensityEffect);
+window.effectLevel.addEventListener('click', changeIntensityEffect);
 
 // 2.1. Масштаб:
 var MIN_SCALE = 0;
 var MAX_SCALE = 100;
 var STEP = 25;
-var scaleControlSmaller = imgUploadOverlayElement.querySelector(
+var scaleControlSmaller = window.imgUploadOverlayElement.querySelector(
     '.scale__control--smaller'
 );
-var scaleControlBigger = imgUploadOverlayElement.querySelector(
+var scaleControlBigger = window.imgUploadOverlayElement.querySelector(
     '.scale__control--bigger'
 );
 
 var outZoom = function () {
-  var scaleSmaller = parseInt(scaleCntrolValue.value, 10) - STEP;
+  var scaleSmaller = parseInt(window.scaleCntrolValue.value, 10) - STEP;
 
   if (scaleSmaller <= MIN_SCALE) {
     scaleSmaller = MIN_SCALE;
   }
-  scaleCntrolValue.value = scaleSmaller + '%';
+  window.scaleCntrolValue.value = scaleSmaller + '%';
 
-  imageUploadPreviewElement.style.transform =
+  window.imageUploadPreviewElement.style.transform =
     'scale(' + scaleSmaller / 100 + ')';
 };
 
 var inZoom = function () {
-  var scaleBigger = parseInt(scaleCntrolValue.value, 10) + STEP;
+  var scaleBigger = parseInt(window.scaleCntrolValue.value, 10) + STEP;
 
   if (scaleBigger >= MAX_SCALE) {
     scaleBigger = MAX_SCALE;
   }
-  scaleCntrolValue.value = scaleBigger + '%';
+  window.scaleCntrolValue.value = scaleBigger + '%';
 
-  imageUploadPreviewElement.style.transform =
+  window.imageUploadPreviewElement.style.transform =
     'scale(' + scaleBigger / 100 + ')';
 };
 
