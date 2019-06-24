70913b19feb12555a938caf242e7eb3a6e25781a
Andrey-Demkovich <a.demkovich@tut.by>
2019-06-24 21:25:07 +0300

    В effects-intensity.js заменил if else на switch

--- js/effects-intensity.js
+++ js/effects-intensity.js
@@ -9,35 +9,29 @@
         window.effectLevelLineElement.clientWidth
       ).toFixed(2) * 100;
 
-    if (
-      window.imageUploadPreviewElement.classList[0] ===
-      'effects__preview--chrome'
-    ) {
-      window.imageUploadPreviewElement.style.filter =
-        'grayscale(' + window.effectLevelValueElement.value / 100 + ')';
-    } else if (
-      window.imageUploadPreviewElement.classList[0] ===
-      'effects__preview--sepia'
-    ) {
-      window.imageUploadPreviewElement.style.filter =
-        'sepia(' + window.effectLevelValueElement.value / 100 + ')';
-    } else if (
-      window.imageUploadPreviewElement.classList[0] ===
-      'effects__preview--marvin'
-    ) {
-      window.imageUploadPreviewElement.style.filter =
-        'invert(' + window.effectLevelValueElement.value + '%)';
-    } else if (
-      window.imageUploadPreviewElement.classList[0] ===
-      'effects__preview--phobos'
-    ) {
-      window.imageUploadPreviewElement.style.filter =
-        'blur(' + (window.effectLevelValueElement.value / 100) * 3 + 'px)';
-    } else if (
-      window.imageUploadPreviewElement.classList[0] === 'effects__preview--heat'
-    ) {
-      window.imageUploadPreviewElement.style.filter =
-        'brightness(' + (window.effectLevelValueElement.value / 100) * 3 + ')';
+    switch (window.imageUploadPreviewElement.classList[0]) {
+      case 'effects__preview--chrome':
+        window.imageUploadPreviewElement.style.filter =
+          'grayscale(' + window.effectLevelValueElement.value / 100 + ')';
+        break;
+      case 'effects__preview--sepia':
+        window.imageUploadPreviewElement.style.filter =
+          'sepia(' + window.effectLevelValueElement.value / 100 + ')';
+        break;
+      case 'effects__preview--marvin':
+        window.imageUploadPreviewElement.style.filter =
+          'invert(' + window.effectLevelValueElement.value + '%)';
+        break;
+      case 'effects__preview--phobos':
+        window.imageUploadPreviewElement.style.filter =
+          'blur(' + (window.effectLevelValueElement.value / 100) * 3 + 'px)';
+        break;
+      case 'effects__preview--heat':
+        window.imageUploadPreviewElement.style.filter =
+          'brightness(' +
+          (window.effectLevelValueElement.value / 100) * 3 +
+          ')';
+        break;
     }
   };
 })();
