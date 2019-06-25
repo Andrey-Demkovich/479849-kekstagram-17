67b98845f9ffd6ab051f29fb343ce2cf0d5f6d6a
Andrey-Demkovich <a.demkovich@tut.by>
2019-06-25 13:46:04 +0300

    Убрал дублирование кода в slider-drag-drop.js

--- js/slider-drag-drop.js
+++ js/slider-drag-drop.js
@@ -2,64 +2,58 @@
 // Перетаскивание слайдера эффектов
 
 (function () {
+  // Перемещаем ползунок слайдера (для перетаскивания или клика)
+  window.onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    if (startCoordsX === undefined) {
+      var startCoordsX = window.effectLevelPinElement.getBoundingClientRect()
+        .left;
+    }
+
+    var shiftX = startCoordsX - moveEvt.clientX;
+    startCoordsX = moveEvt.clientX;
+
+    var pinElementLeft = window.effectLevelPinElement.offsetLeft - shiftX;
+
+    var lineElementLeft = window.effectLevelLineElement.getBoundingClientRect()
+      .left;
+    var lineElementRight = window.effectLevelLineElement.getBoundingClientRect()
+      .right;
+    if (startCoordsX <= lineElementLeft) {
+      pinElementLeft = 0;
+    } else if (startCoordsX >= lineElementRight) {
+      pinElementLeft = window.effectLevelLineElement.clientWidth;
+    }
+
+    window.effectLevelPinElement.style.left = pinElementLeft + 'px';
+    window.effectLevelDepthElement.style.width = pinElementLeft + 'px';
+  };
+
+  // Drag-end-drop
   window.effectLevelPinElement.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
     var startCoordsX = evt.clientX;
 
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      var shiftX = startCoordsX - moveEvt.clientX;
-      startCoordsX = moveEvt.clientX;
-
-      var pinElementLeft = window.effectLevelPinElement.offsetLeft - shiftX;
-
-      var lineElementLeft = window.effectLevelLineElement.getBoundingClientRect()
-        .left;
-      var lineElementRight = window.effectLevelLineElement.getBoundingClientRect()
-        .right;
-      if (startCoordsX <= lineElementLeft) {
-        pinElementLeft = 0;
-      } else if (startCoordsX >= lineElementRight) {
-        pinElementLeft = window.effectLevelLineElement.clientWidth;
-      }
-
-      window.effectLevelPinElement.style.left = pinElementLeft + 'px';
-      window.effectLevelDepthElement.style.width = pinElementLeft + 'px';
-    };
-
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
 
-      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mousemove', window.onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
     };
 
-    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mousemove', window.onMouseMove);
     document.addEventListener('mousemove', window.changeIntensityEffect);
 
     document.addEventListener('mouseup', onMouseUp);
   });
 
-  // Изменение положения и интенсивности эффекта по клику на слайдер
-  window.effectLevel.addEventListener('click', function (clickEvt) {
-    var coordsX = clickEvt.clientX;
-    var lineElementLeft = window.effectLevelLineElement.getBoundingClientRect()
+  // Изменение положения ползунка и интенсивности эффекта по клику на слайдер
+  window.effectLevel.addEventListener('click', function (evt) {
+    var startCoordsX = window.effectLevelPinElement.getBoundingClientRect()
       .left;
-    var lineElementRight = window.effectLevelLineElement.getBoundingClientRect()
-      .right;
-
-    var elementLeft = coordsX - lineElementLeft;
-
-    if (coordsX <= lineElementLeft) {
-      elementLeft = 0;
-    } else if (coordsX >= lineElementRight) {
-      elementLeft = window.effectLevelLineElement.clientWidth;
-    }
-
-    window.effectLevelPinElement.style.left = elementLeft + 'px';
-    window.effectLevelDepthElement.style.width = elementLeft + 'px';
+    window.onMouseMove(evt);
   });
 
   window.effectLevel.addEventListener('click', window.changeIntensityEffect);
