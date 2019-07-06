d8b39eb02de07fb1f92b9631ccf16e537fb8599f
Andrey-Demkovich <a.demkovich@tut.by>
2019-07-06 19:08:47 +0300

    Набросал алгоритм вывода 5 сообщений или меньше

--- js/big-picture-open-close.js
+++ js/big-picture-open-close.js
@@ -1,30 +1,51 @@
 'use strict';
+// Открытие/закрытие большой фотографии из загруженной галереи
 
 (function () {
   var bigPictureCancelElement = window.bigPictureElement.querySelector(
       '.big-picture__cancel'
   );
 
-  var closebigPicture = function () {
-    window.bigPictureElement.classList.add('hidden');
+  // Обработчик закрытия при нажатии Esc
+  var onBigPictureEscPress = function (evt) {
+    if (evt.keyCode === 27) {
+      closeBigPicture();
+    }
   };
 
-  bigPictureCancelElement.addEventListener('click', closebigPicture);
+  // Обработчик закрытия большого фото
+  var closeBigPicture = function () {
+    window.bigPictureElement.classList.add('hidden');
 
-  window.picturesContainerElement.addEventListener('click', function (evt) {
+    // Удаляем обработчик закрытия при нажатии Esc
+    document.removeEventListener('keydown', onBigPictureEscPress);
+  };
+
+  // Обработчик открытия большого фото
+  var openBigPicture = function (evt) {
+    // Определяем элемент на котором произошло событие
     var target = evt.target;
-    console.log('target:', target);
+    // Определяем произошло событие на .picture или на каком-то вложенном элементе
     var picture = target.closest('.picture');
-    console.log('picture:', picture);
+    // Если клик не на .picture элементе то выходим из функции
     if (!picture) {
       return;
     }
 
-    var fil = window.XhrDataImgPosts.filter(function (object) {
-      return object.url === target.getAttribute('src');
+    // Фильтруем загруженные данные и определяем по src кликнутой картинки какой объект с данными нужен для отрисовки большого фото
+    var filterDataElement = window.XhrDataImgPosts.filter(function (object) {
+      return object.url === picture.querySelector('img').getAttribute('src');
     })[0];
-    console.log(fil);
+    // Заполняет данные для просмотра фото в полноразмерном режиме
+    window.createBigPicture(filterDataElement);
+    // Открываем большое фото
     window.bigPictureElement.classList.remove('hidden');
-    window.createBigPicture(fil);
-  });
+
+    // При открытии большого фото, добовляем события его закрытия
+    bigPictureCancelElement.addEventListener('click', closeBigPicture);
+    document.addEventListener('keydown', onBigPictureEscPress);
+  };
+
+  // Событие открытия большого изображения по клику (использует делегирование)
+  window.picturesContainerElement.addEventListener('click', openBigPicture);
 })();
--- js/big-picture-post.js
+++ js/big-picture-post.js
@@ -11,24 +11,59 @@
   var commentsCountElement = window.bigPictureElement.querySelector(
       '.comments-count'
   );
+  var socialCommentContainerElement = window.bigPictureElement.querySelector(
+      '.social__comments'
+  );
   var socialCommentElements = window.bigPictureElement.querySelectorAll(
+      '.social__comment'
+  );
+  var socialCommentElement = window.bigPictureElement.querySelector(
       '.social__comment'
   );
   var socialCaptionElement = window.bigPictureElement.querySelector(
       '.social__caption'
   );
 
+  var createCommentElement = function (bigPictureData) {
+    console.log(bigPictureData);
+    var commentElement = socialCommentElement.cloneNode(true);
+    var fragmentComment = document.createDocumentFragment();
+    // for (var i = 0; i < 5; i++)
+    var i = 0;
+    var commentElements = [];
+    while (
+      commentElements.length < 5 &&
+      commentElements.length < bigPictureData.comments.length
+    ) {
+      commentElement.querySelector('.social__picture').src =
+        bigPictureData.comments[i].avatar;
+      commentElement.querySelector('.social__text').textContent =
+        bigPictureData.comments[i].message;
+      fragmentComment.appendChild(commentElement);
+      console.log(fragmentComment);
+      commentElements = fragmentComment.querySelectorAll('.social__comment');
+      commentElement = commentElement.cloneNode(true);
+      i++;
+    }
+
+    socialCommentContainerElement.innerHTML = '';
+    socialCommentContainerElement.appendChild(fragmentComment);
+  };
+
   // Заполняет данные при просмотре фотографий в полноразмерном режиме
   window.createBigPicture = function (bigPictureData) {
     bigPictureImgElement.src = bigPictureData.url;
     likesCountElement.textContent = bigPictureData.likes;
     commentsCountElement.textContent = bigPictureData.comments.length;
-    socialCommentElements.forEach(function (element, i) {
-      element.querySelector('.social__picture').src =
-        bigPictureData.comments[i].avatar;
-      element.querySelector('.social__text').textContent =
-        bigPictureData.comments[i].message;
-    });
+
+    createCommentElement(bigPictureData);
+
+    // socialCommentElements.forEach(function (element, i) {
+    //   element.querySelector('.social__picture').src =
+    //     bigPictureData.comments[i].avatar;
+    //   element.querySelector('.social__text').textContent =
+    //     bigPictureData.comments[i].message;
+    // });
     socialCaptionElement.textContent = bigPictureData.description;
     window.bigPictureElement
       .querySelector('.social__comment-count')
